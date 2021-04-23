45b7ef4824efdf3215d9518d2bac54ac85abc18f
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-04-23 14:06:04 +0300

    1.6. Added 'primite types' usage.

---
+++ lesson_1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte coreQuantity = 1;
+        short cost = 30_000;
+        int starQuantity = 1_000_000_000;
+        long microbeQuantity = 3_000_000_000l;
+        float pi = 3.14f;
+        double exactPi = 3.14159d;
+        char firstLetter = 'A';
+        boolean isWorking = false;
+        System.out.println(coreQuantity);
+        System.out.println(cost);
+        System.out.println(starQuantity);
+        System.out.println(microbeQuantity);
+        System.out.println(pi);
+        System.out.println(exactPi);
+        System.out.println(firstLetter);
+        System.out.println(isWorking);
+    }
+}
