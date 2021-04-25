6937fed104440cfbb3af84d9d6e2e47f3927690d
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-04-25 17:20:06 +0300

    2.6. Added 'Jaeger'.

---
+++ lesson_2/Jaeger.java
@@ -0,0 +1,36 @@
+public class Jaeger {
+    private String name;
+    private double height;
+
+    public Jaeger() {
+    }
+
+    public Jaeger(String name, double height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void move() {
+        System.out.println("I'm moving.");
+    }
+
+    public void fire() {
+        System.out.println("I'm firing.");
+    }
+}
---
+++ lesson_2/JaegerTest.java
@@ -0,0 +1,23 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger killer = new Jaeger();
+        killer.setName("killer");
+        killer.setHeight(1.78d);
+
+        System.out.println(killer.getName());
+        killer.setName("super-killer");
+        System.out.println(killer.getName());
+        System.out.println(killer.getHeight());
+        killer.move();
+        killer.fire();
+
+        System.out.println("\n============\n");
+
+        Jaeger flyer = new Jaeger("flyer", 0.1d);
+
+        System.out.println(flyer.getName());
+        System.out.println(flyer.getHeight());
+        flyer.move();
+        flyer.fire();
+    }
+}
