3a193e6908dcb7677503b91720b8a38767229615
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-04-25 15:16:12 +0300

    2.2. Added 'Person' class.

---
+++ lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    boolean isMale = false;
+    String name = "Olya";
+    int height = 170;
+    double weight = 55.0d;
+    byte age = 24;
+
+    void walk() {
+        System.out.println("I'm walking.");
+    }
+
+    void sit() {
+        System.out.println("I'm sitting.");
+    }
+
+    void jog() {
+        System.out.println("I'm jogging.");
+    }
+
+    void speak() {
+        System.out.println("I'm speaking.");
+    }
+
+    void learnJava() {
+        System.out.println("I'm learning Java.");
+    }
+}
