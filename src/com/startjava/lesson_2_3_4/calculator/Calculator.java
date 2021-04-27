c69ea5bf744c9fa993d2a5f10adc2f07c0e444db
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-04-27 12:38:33 +0300

    4. Rename folder.

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,9 +1,9 @@
-package com.startjava.lesson_2_3.calculator;
+package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
     public int calculate(char sign, int firstNumber, int secondNumber) {
         int result = 0;
-        
+
         switch(sign) {
             case '+':
                 result = firstNumber + secondNumber;
