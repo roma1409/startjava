65aa5897b49a9124852b2c77edd39f886f0261ea
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-04-29 11:24:43 +0300

    5. Added script to create DB.

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,13 @@
+-- DROP DATABASE IF EXISTS jaegers;
+-- CREATE DATABASE jaegers;
+
+CREATE TABLE IF NOT EXISTS jaegers (
+    id SERIAL PRIMARY KEY ,
+    model_name VARCHAR(20),
+    mark VARCHAR(20),
+    height DOUBLE PRECISION,
+    weight DOUBLE PRECISION,
+    status VARCHAR(20),
+    origin VARCHAR(20),
+    launch DATE,
+    kaiju_kill integer);
