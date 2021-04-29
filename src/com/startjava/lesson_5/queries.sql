018244af12bf3522f48e95e595613dfc670c7077
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-04-29 13:54:55 +0300

    5. Added script for some queries.

---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,34 @@
+SELECT *
+FROM jaegers;
+
+SELECT *
+FROM jaegers
+WHERE status != 'Destroyed';
+
+SELECT *
+FROM jaegers
+WHERE mark = 'Mark-1'
+   OR mark = 'Mark-4';
+
+SELECT *
+FROM jaegers
+ORDER BY mark DESC;
+
+SELECT *
+FROM jaegers
+WHERE launch = (SELECT MIN(launch) FROM jaegers);
+
+SELECT *
+FROM jaegers
+WHERE kaiju_kill IN ((SELECT MAX(kaiju_kill) FROM jaegers), (SELECT MIN(kaiju_kill) FROM jaegers));
+
+SELECT AVG(weight)
+FROM jaegers;
+
+UPDATE jaegers
+SET kaiju_kill = kaiju_kill + 1
+WHERE status != 'Destroyed';
+
+DELETE
+FROM jaegers
+WHERE status = 'Destroyed';
